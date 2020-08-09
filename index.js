8cbd91726d2080f9cf203791a9e4c8ebfd81d0dc
rm20killer <43253883+rm20killer@users.noreply.github.com>
2020-08-09 17:00:20 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const HytaleApi = require('hytale-api-sdk');
+
+const api = new HytaleApi.ArticlesApi();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if(msg.content === '!ping') {
+        msg.reply('pong');
+    }
+    if (msg.content === '!blog') {
+        if (msg.member.roles.cache.find(r => r.name === '⁣       Staff Team       ⁣')) {
+        console.log('!blog');
+
+        api.getArticles({}, function(error, data, response) {
+            if (error) {
+                console.error(error);
+                } else {
+                    // get 3 latest articles
+                    const latest = data.slice(0, 1);
+                    latest.forEach(function(article) {
+                        const publishAt = new Date(article.publishedAt);
+                        const embed = new Discord.MessageEmbed()
+                            .setColor(3447003)
+                            .setThumbnail(`https://hytale.com/m/variants/blog_thumb_${article.coverImage.s3Key}`)
+                            .setAuthor('Hytale', 'https://cdn.discordapp.com/attachments/741665332808122469/741685930401857596/G-O9yHTX.jpg')
+                            .setTitle(article.title)
+                            .setDescription(article.bodyExcerpt)
+                            .setURL(`https://hytale.com/news/${publishAt.getFullYear()}/${publishAt.getMonth() + 1}/${article.slug}`)
+                            .addField('Published At', `${publishAt.toLocaleDateString()}`)
+                        ;  
+
+                        msg.channel.send(embed);
+                    });
+                }
+            });
+        }
+    }
+});
+
+client.login('NzQxNjgyNDY3ODkwNTI4NDI5.Xy7HoA.ewMaQ2-5YbZggW8a_hjRMiysIH0');
